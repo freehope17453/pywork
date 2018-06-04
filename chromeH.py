6c3f73e63a52f6e2aeb6b4c9a1a010592321c8cb
freehope17453 <freehope_9@live.com>
2018-06-04 16:24:09 +0800

    selenuim+chrome

---
+++ chromeH.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+driver.get("http://cn.bing.com/")
+#driver.quit()
+driver.find_element_by_xpath("//input[@id='sb_form_q']").send_keys("李晓鹏")
+driver.find_element_by_xpath("//input[@id='sb_form_go']").click()
